ed8a5aabba4b3591813ad6de1fb6e6aefe35d7fd
theshubhamy <devshubhamyadav@gmail.com>
2023-05-15 12:21:41 +0530

    setup

---
+++ src/main/java/com/example/myshowapi/MyshowapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshowapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myshowapi/MyshowapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshowapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
